53d7ef837acba9a252aa4e6ad969406a25f1ce12
Robert <robert@bsdroot.com>
2014-09-17 11:44:33 +0200

    Moved and bumped up os x sdk version

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 CC=clang
 INCLUDE="-I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/darwin"
 FRAMEWORK="-framework Cocoa"
-SRC="native/main.m"
+SRC="src/main/objc/main.m"
 DST="src/main/resources/com/bsdroot/github/appbundler/JavaAppLauncher"
-SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
 ARCH="x86_64"
-MIN_MAC_OSX_VERSION="10.7"
+MIN_MAC_OSX_VERSION="10.9"
 
 $CC -o $DST $FRAMEWORK $INCLUDE -arch $ARCH -isysroot $SDK -mmacosx-version-min=$MIN_MAC_OSX_VERSION $SRC
